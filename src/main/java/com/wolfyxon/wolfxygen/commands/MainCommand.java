8195856bbe79bac862de73e86f33b129b876a0fe
Wolfyxon <szymon.sajmon.b@gmail.com>
2023-06-07 15:55:00 +0200

    Initial MainCommand

---
+++ src/main/java/com/wolfyxon/wolfxygen/commands/MainCommand.java
@@ -0,0 +1,16 @@
+package com.wolfyxon.wolfxygen.commands;
+
+import com.wolfyxon.wolfxygen.Wolfxygen;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+
+public class MainCommand extends WolfxygenCommand{
+    public MainCommand(Wolfxygen plugin) {
+        super(plugin);
+    }
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        sender.sendMessage("test");
+        return false;
+    }
+}
