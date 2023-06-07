b2ff76f2599072ab5780cd97822b56163810d1b2
Wolfyxon <szymon.sajmon.b@gmail.com>
2023-06-07 16:46:05 +0200

    Initial Action system

--- src/main/java/com/wolfyxon/wolfxygen/commands/MainCommand.java
+++ src/main/java/com/wolfyxon/wolfxygen/commands/MainCommand.java
@@ -4,10 +4,31 @@ import com.wolfyxon.wolfxygen.Wolfxygen;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 
+import java.util.Arrays;
+
 public class MainCommand extends WolfxygenCommand{
     public MainCommand(Wolfxygen plugin) {
         super(plugin);
     }
+
+    class Action {
+        public String[] aliases;
+        public String description;
+        public String permission;
+
+        public Action(String[] aliases, String description, String permission){
+            this.aliases = aliases;
+            this.description = description;
+            this.permission = permission;
+        }
+        public Action(String alias, String description, String permission){
+            this(new String[]{alias},description,permission);
+        }
+
+        public boolean hasAlias(String alias){ return Arrays.asList(aliases).contains(alias); }
+
+    }
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if(args.length == 0){
@@ -16,6 +37,8 @@ public class MainCommand extends WolfxygenCommand{
             sendMsg(sender,"&6https://github.com/Wolfyxon/Wolfxygen");
             return true;
         }
+        String action = args[0];
+
         return true;
     }
 }
