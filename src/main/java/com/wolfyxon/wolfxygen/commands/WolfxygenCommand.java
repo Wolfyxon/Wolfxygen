c94a6b9b2054c2bbfff28a9019ec7f5e7a7a714a
Wolfyxon <szymon.sajmon.b@gmail.com>
2023-06-07 16:02:28 +0200

    Config var

--- src/main/java/com/wolfyxon/wolfxygen/commands/WolfxygenCommand.java
+++ src/main/java/com/wolfyxon/wolfxygen/commands/WolfxygenCommand.java
@@ -1,5 +1,6 @@
 package com.wolfyxon.wolfxygen.commands;
 
+import com.wolfyxon.wolfxygen.ConfigMgr;
 import com.wolfyxon.wolfxygen.Wolfxygen;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -7,8 +8,10 @@ import org.bukkit.command.CommandSender;
 
 public class WolfxygenCommand implements CommandExecutor {
     Wolfxygen plugin;
+    ConfigMgr config;
     public WolfxygenCommand(Wolfxygen plugin){
         this.plugin = plugin;
+        config = plugin.config;
     }
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
