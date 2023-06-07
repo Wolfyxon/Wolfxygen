0bcd466bda48951458f9d618f32cf6f79470b842
Wolfyxon <szymon.sajmon.b@gmail.com>
2023-06-07 15:59:02 +0200

    Command registering

--- src/main/java/com/wolfyxon/wolfxygen/Wolfxygen.java
+++ src/main/java/com/wolfyxon/wolfxygen/Wolfxygen.java
@@ -1,5 +1,7 @@
 package com.wolfyxon.wolfxygen;
 
+import com.wolfyxon.wolfxygen.commands.MainCommand;
+import com.wolfyxon.wolfxygen.commands.WolfxygenCommand;
 import com.wolfyxon.wolfxygen.events.PlayerEventsListener;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -19,12 +21,17 @@ public final class Wolfxygen extends JavaPlugin {
 
         getServer().getPluginManager().registerEvents(playerEvents,this);
 
+        registerCommand("wolfxygen", new MainCommand(this));
+
         getLogger().info("Enabled");
     }
     public void loadConfig(){
         oxygenMgr.applyConfig();
         oxygenMgr.start();
         getLogger().info("Config has been loaded");
+    }
+    public void registerCommand(String name, WolfxygenCommand executor){
+        getCommand(name).setExecutor(executor);
     }
 
 
