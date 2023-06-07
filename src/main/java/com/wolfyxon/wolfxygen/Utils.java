b8c74539c4b301c94b511d27f6474c84c4897828
Wolfyxon <szymon.sajmon.b@gmail.com>
2023-06-07 15:34:48 +0200

    multiReplace() for replacing multitple strings at once

--- src/main/java/com/wolfyxon/wolfxygen/Utils.java
+++ src/main/java/com/wolfyxon/wolfxygen/Utils.java
@@ -4,9 +4,17 @@ import net.md_5.bungee.api.ChatMessageType;
 import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.entity.Player;
 
+import java.util.Map;
+
 public class Utils {
     public static void showActionBar(Player player, String text){
         if(text == null) return;
         player.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(text));
+    }
+    public static String multiReplace(String str, Map<String,String> replacements){
+        for(Map.Entry e : replacements.entrySet()){
+            str = str.replace( (String) e.getKey(), (String) e.getValue());
+        }
+        return str;
     }
 }
