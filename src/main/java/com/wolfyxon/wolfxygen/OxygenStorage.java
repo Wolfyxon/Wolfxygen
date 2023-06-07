15fa95621d2b5c91aceb49b460054c1971d2794a
Wolfyxon <szymon.sajmon.b@gmail.com>
2023-06-07 19:08:14 +0200

    Individual loading and saving

--- src/main/java/com/wolfyxon/wolfxygen/OxygenStorage.java
+++ src/main/java/com/wolfyxon/wolfxygen/OxygenStorage.java
@@ -43,14 +43,17 @@ public class OxygenStorage {
     public void setOxygen(UUID uuid, double oxygen){ setOxygen(uuid.toString(), oxygen); }
     public void setOxygen(Player player, double oxygen){ setOxygen(player.getUniqueId(), oxygen); }
 
+    public void save(Player player){ setOxygen(player, oxygenMgr.getOxygen(player) ); }
+    public void load(Player player){ oxygenMgr.setOxygen(player, getOxygen(player) ); }
+
     public void saveAll(){
         for(Player plr : plugin.getServer().getOnlinePlayers()){
-            setOxygen(plr, oxygenMgr.getOxygen(plr) );
+            save(plr);
         }
     }
     public void loadAll(){
         for(Player plr : plugin.getServer().getOnlinePlayers()){
-            oxygenMgr.setOxygen(plr, getOxygen(plr) );
+            load(plr);
         }
     }
 
