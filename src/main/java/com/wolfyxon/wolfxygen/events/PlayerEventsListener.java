0904adcb8a13c9ac7f3967a7308c3bc0ea9e7244
Wolfyxon <szymon.sajmon.b@gmail.com>
2023-06-07 14:37:38 +0200

    Join and leave events

--- src/main/java/com/wolfyxon/wolfxygen/events/PlayerEventsListener.java
+++ src/main/java/com/wolfyxon/wolfxygen/events/PlayerEventsListener.java
@@ -3,16 +3,29 @@ package com.wolfyxon.wolfxygen.events;
 import com.wolfyxon.wolfxygen.Wolfxygen;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
 
 public class PlayerEventsListener implements Listener {
     Wolfxygen plugin;
-    public PlayerEventsListener(Wolfxygen plugin){
+
+    public PlayerEventsListener(Wolfxygen plugin) {
         this.plugin = plugin;
     }
+
     @EventHandler
     public void onRespawn(PlayerRespawnEvent q) {
-        plugin.oxygenMgr.setOxygen(q.getPlayer(),plugin.oxygenMgr.maxOxygen);
+        plugin.oxygenMgr.setOxygen(q.getPlayer(), plugin.oxygenMgr.maxOxygen);
     }
 
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent e) {
+
+    }
+
+    @EventHandler
+    public void onPlayerLeave(PlayerQuitEvent event) {
+
+    }
 }
